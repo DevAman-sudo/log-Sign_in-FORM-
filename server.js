7708c31a84e323ea84b50732ca21f6390b345c77
DevAman-sudo <devaman.net@gmail.com>
2021-01-11 19:16:22 +0545

    wrote basic frontend and backend codes

---
+++ server.js
@@ -0,0 +1,26 @@
+// NPM packages and app setup //
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const Datastore = require('nedb');
+const chalk = require('chalk');
+const path = require('path');
+const port = process.env.PORT || 8080;
+
+// file path || app use //
+const staticPath = path.join( __dirname , 'public/');
+app.use(express.static(staticPath));
+
+// server / app routing //
+app.get('/' , (req , res) => {
+    res.sendFile( path.join(staticPath , 'index.html'));
+});
+
+// listening on server port 8080 //
+server.listen( port , (err) => {
+    if (err) {
+        console.log(`Error Found => ${err}`);
+    } else {
+        console.log( chalk.red.bgBlue.bold(`http://127.0.0.1:8080`));
+    }
+});
