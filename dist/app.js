50d2263bbe0fead1def58915e5f9a5cd240b40b6
DevAman-sudo <devaman.net@gmail.com>
2021-01-15 20:10:51 +0545

    installed web_socket

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = express_1.default();
+const port = process.env.PORT || 8080;
+const staticPath = path_1.default.join(__dirname, '../public/');
+app.use(express_1.default.static(staticPath));
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.sendFile(path_1.default.join(staticPath, 'index.html'));
 });
-app.listen(8080, () => {
+app.listen(port, () => {
     console.log('server started');
 });
--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import WebSocket from 'socket.io';
+import path from 'path';
 
 const app: express.Application = express();
+const port = process.env.PORT || 8080;
+
+const staticPath = path.join(__dirname , '../public/');
+
+app.use(express.static(staticPath));
 
 app.get('/' , (req , res) => {
-    res.send('hello world');
+    res.sendFile(path.join(staticPath , 'index.html'));
 });
 
-app.listen(8080 , () => {
+app.listen(port , () => {
     console.log('server started');
 });
