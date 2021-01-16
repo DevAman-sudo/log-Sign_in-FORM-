73aaa3816a1e42e21505e3d34fc0b0d720753571
DevAman-sudo <devaman.net@gmail.com>
2021-01-16 10:43:31 +0545

    working on backend routing

--- dist/app.js
+++ dist/app.js
@@ -3,15 +3,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// NPM packages //
 const express_1 = __importDefault(require("express"));
+const chalk_1 = __importDefault(require("chalk"));
 const path_1 = __importDefault(require("path"));
+// app and port setup //
 const app = express_1.default();
 const port = process.env.PORT || 8080;
+// file path //
 const staticPath = path_1.default.join(__dirname, '../public/');
 app.use(express_1.default.static(staticPath));
+// app route //
 app.get('/', (req, res) => {
     res.sendFile(path_1.default.join(staticPath, 'index.html'));
 });
+// listening to server on port 8080 //
 app.listen(port, () => {
-    console.log('server started');
+    console.log(chalk_1.default.red.bgBlue.bold(`http://127.0.0.1:${port}`));
 });
--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,24 @@
-import express from 'express';
+// NPM packages //
+import express, {Application , Request , Response , NextFunction} from 'express';
 import WebSocket from 'socket.io';
+import chalk from 'chalk';
 import path from 'path';
 
-const app: express.Application = express();
+// app and port setup //
+const app: Application = express();
 const port = process.env.PORT || 8080;
 
+// file path //
 const staticPath = path.join(__dirname , '../public/');
 
 app.use(express.static(staticPath));
 
-app.get('/' , (req , res) => {
+// app route //
+app.get('/' , (req: Request , res: Response) => {
     res.sendFile(path.join(staticPath , 'index.html'));
 });
 
+// listening to server on port 8080 //
 app.listen(port , () => {
-    console.log('server started');
+    console.log( chalk.red.bgBlue.bold(`http://127.0.0.1:${port}`));
 });
