146a98cfc9a53134535c586ae10d2f8ff746b4eb
DevAman-sudo <devaman.net@gmail.com>
2021-01-18 20:38:47 +0545

    client side programming

---
+++ public/js/index.js
@@ -0,0 +1,11 @@
+// DOM elements //
+const form = document.getElementById('form');
+const textArea = document.getElementById('text-area');
+const button = document.getElementById('button');
+const dataContainer = document.getElementById('data-container');
+
+// listening submit event on form submit //
+form.addEventListener('submit' , (event) => {
+    // prevent page to submit //
+    event.preventDefault();
+});
